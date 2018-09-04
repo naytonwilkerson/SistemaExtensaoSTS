543ca5ad79c82d376b9eabeb2ce1e0eeb7349b8f
NaytonWilkerson <naytonwilkerson78@gmail.com>
2018-09-04 11:28:42 -0300

    Versão inicial do projeto

---
+++ src/main/java/br/edu/iftm/extensao/SistemaExtensaoStsApplication.java
@@ -0,0 +1,12 @@
+package br.edu.iftm.extensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExtensaoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExtensaoStsApplication.class, args);
+	}
+}
